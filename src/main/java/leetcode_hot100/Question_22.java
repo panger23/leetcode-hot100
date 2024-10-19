a28be3bfb3abde8e5d3600dc2686934a85d6f831
chilianpan <2725574310@qq.com>
2024-10-19 19:37:51 +0800

    力扣学习

---
+++ src/main/java/leetcode_hot100/Question_22.java
@@ -0,0 +1,18 @@
+package leetcode_hot100;
+
+public class Question_22 {
+    public static void main(String[] args) {
+
+    }
+    public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+    }
+}
+ class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+       val = x;
+       next = null;
+    }
+ }
