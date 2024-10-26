db336ae99944e193a753f49ac9416f13f8dad324
chilianpan <2725574310@qq.com>
2024-10-27 00:43:19 +0800

    已经完成31题，hard跳过

---
+++ src/main/java/leetcode_hot100/Question_31.java
@@ -0,0 +1,37 @@
+package leetcode_hot100;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+
+class Question_31 {
+    public Node copyRandomList(Node head) {
+        if(head == null) return null;
+        Node cur = head; // 记录当前指向的节点
+        Map<Node,Node> map = new HashMap<>(); // 集合映射原链表和新链表
+        while(cur != null){
+            Node temp = new Node(cur.val);
+            map.put(cur,temp);
+            cur = cur.next;
+        }
+        cur = head;
+        while(cur != null){
+            map.get(cur).next = map.get(cur.next);
+            map.get(cur).random = map.get(cur.random);
+            cur = cur.next;
+        }
+        return map.get(head);
+    }
+}
